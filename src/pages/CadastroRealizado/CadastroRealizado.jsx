3a90fd3dca8f73079c18a0440c7c7618da01ac3a
Théo Barreto Silva <barretotheo25@gmail.com>
2024-06-21 22:02:41 -0300

    design tela de cadastro realizada pronto

--- src/pages/CadastroRealizado/CadastroRealizado.jsx
+++ src/pages/CadastroRealizado/CadastroRealizado.jsx
@@ -1,12 +1,14 @@
-import { useContext } from "react"
-import { UserContext } from "../../context/UserContext"
+// import { useContext } from "react"
+// import { UserContext } from "../../context/UserContext"
+import * as S from './CadastroRealizado.styles'
 
 export function CadastroRealizado() {
-    const {user, setUser} = useContext(UserContext);
+    // const {user, setUser} = useContext(UserContext);
 
     return(
-        <>
-            TELA DE CADASTRO REALIZADO
-        </>
+        <S.MainStyled>
+            <S.TxtCadRea>Cadastro realizado!</S.TxtCadRea>
+            <S.ImgCadRea src='../../src/assets/svg/cadastro_realizado.svg' />
+        </S.MainStyled>
     )
 }
---
+++ src/pages/CadastroRealizado/CadastroRealizado.styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const MainStyled = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+`
+
+export const TxtCadRea = styled.h1`
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 700;
+    font-size: 3rem;
+    text-align: center;
+`
+
+export const ImgCadRea = styled.img`
+    width: 30vw;
+    height: 60vh;
+`
