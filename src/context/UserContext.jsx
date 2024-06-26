8475365726bacac6ece8e5ccfd614646976eab7d
Théo Barreto Silva <barretotheo25@gmail.com>
2024-06-25 21:09:55 -0300

    finalizando UserContext

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,13 +1,18 @@
 import { createContext, useState } from "react";
+import PropTypes from "prop-types";
 
 export const UserContext = createContext();
 
-export const ContextProvider = ({children}) => {
+export const ContextProvider = ({ children }) => {
     const [user, setUser] = useState();
 
-    return(
-        <UserContext.Provider value={user}>
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
             {children}
         </UserContext.Provider>
-    )
-}
+    );
+};
+
+ContextProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
