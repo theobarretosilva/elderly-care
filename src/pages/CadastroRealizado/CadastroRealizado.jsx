eaa300e482fcc874c8c84e4fd34dd7854c56dbf4
Théo Barreto Silva <barretotheo25@gmail.com>
2024-06-25 21:13:31 -0300

    Tela CadastroRealizado finalizada

--- src/pages/CadastroRealizado/CadastroRealizado.jsx
+++ src/pages/CadastroRealizado/CadastroRealizado.jsx
@@ -1,9 +1,17 @@
-// import { useContext } from "react"
-// import { UserContext } from "../../context/UserContext"
+import { useContext } from "react"
+import { UserContext } from "../../context/UserContext"
 import * as S from './CadastroRealizado.styles'
+import { useNavigate } from "react-router";
 
 export function CadastroRealizado() {
-    // const {user, setUser} = useContext(UserContext);
+    const { user } = useContext(UserContext);
+    const navigate = useNavigate()
+
+    if (user == "Idoso") {
+        setTimeout(() => navigate('/inicioIdoso'), 3 * 1000)
+    } else {
+        setTimeout(() => navigate('/InicioCuidador'), 3 * 1000)
+    }
 
     return(
         <S.MainStyled>
